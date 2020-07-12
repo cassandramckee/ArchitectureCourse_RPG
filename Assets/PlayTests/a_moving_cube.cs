fd28d265901fea95b376987833cff527ba48b63d
Scott McKee <scott.j.mckee@gmail.com>
2020-07-12 14:48:31 -0700

    Initial commit

---
+++ Assets/PlayTests/a_moving_cube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class a_moving_cube
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator moving_forward_changes_position()
+        {
+            // Arrange
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = Vector3.zero;
+            
+            
+            for (int i = 0; i < 10; i++)
+            {
+                // Act
+                cube.transform.position += Vector3.forward;
+                yield return null; // Normally wouldn't do that, but we want to see it
+                
+                // Assert asdf
+                Assert.AreEqual(i+1, cube.transform.position.z);
+            }
+            
+
+        }
+    }
+}
---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Three parts to a test
+            // 1) Arrange (most of the code is typically this part)
+            List<string> myStrings = new List<string>();
+            
+            // 2) Act
+            myStrings.Add("Scott's string");
+            myStrings.Add("Scott's second string");
+            myStrings.RemoveAt(0);
+            
+            // 3) Assert
+            Assert.AreEqual(1, myStrings.Count);
+
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
