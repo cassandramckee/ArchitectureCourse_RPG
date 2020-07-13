0f5685e184dc51572ce115a5d9c1d06670fbf630
Scott McKee <scott.j.mckee@gmail.com>
2020-07-12 17:54:11 -0700

    3b checkpoint

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _characterController;
+    private PlayerInput _playerInput;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _playerInput = new PlayerInput();
+    }
+
+    private void Update()
+    {
+        Vector3 movementInput = new Vector3(0f, 0f, _playerInput.Vertical);
+        Vector3 movement = transform.rotation * movementInput;
+        _characterController.SimpleMove(movement);
+    }
+}
+
+public class PlayerInput
+{
+    public float Vertical => Input.GetAxis("Vertical");
+}
