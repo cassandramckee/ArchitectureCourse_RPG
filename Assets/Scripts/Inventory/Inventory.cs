1d5de632bbfd575f8632396783f4195c8647b422
Scott McKee <scott.j.mckee@gmail.com>
2020-07-25 13:21:35 -0700

    5e checkpoint

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,10 +1,15 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField] private Transform _rightHand;
+
     private List<Item> _items = new List<Item>();
     private Transform _itemRoot;
+    
+    public Item ActiveItem { get; private set; }
 
     private void Awake()
     {
@@ -24,5 +29,11 @@ public class Inventory : MonoBehaviour
     {
         // Likely puts item in right spot, fires a bunch of call backs, etc. 
         Debug.Log($"Equipped Item {item.gameObject.name}");
+        item.transform.SetParent(_rightHand);
+        // Note: Performance impact here is fine since we've only done it once.
+        // If doing repeatedly, cache the value and go from there.
+        item.transform.localPosition = Vector3.zero;
+        item.transform.localRotation = Quaternion.identity;
+        ActiveItem = item;
     }
 }
